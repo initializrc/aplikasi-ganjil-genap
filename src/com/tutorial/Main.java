e003f18f647af8855b4f1da8ecd6229d5542af6f
pangerankode <panji.cahyountoro@gmail.com>
2020-07-30 16:15:34 +0700

    init

---
+++ src/com/tutorial/Main.java
@@ -0,0 +1,20 @@
+package com.tutorial;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("====G A N J I L - G E N A P====");
+        System.out.println("===============================");
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Inputan Nilai = ");
+        int value = input.nextInt();
+
+        if (value % 2 > 0){
+            System.out.println(value + " merupakan bilangan ganji");
+        }else {
+            System.out.println(value + " merupakan bilangan genap");
+        }
+    }
+}
